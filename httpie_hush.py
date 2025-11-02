07381f66e268dea2444d343b3f11aa2d22a650e9
Gilad Sever <gilad@hush.security>
2025-11-02 14:49:39 +0200

    poetry: Update to version 1.2

--- httpie_hush.py
+++ httpie_hush.py
@@ -16,7 +16,7 @@ from httpie.plugins import AuthPlugin
 from httpie.status import ExitStatus
 from requests.auth import HTTPBasicAuth
 
-__version__ = "1.1"
+__version__ = "1.2"
 __author__ = "Gilad Sever"
 __licence__ = "Apache 2.0"
 
